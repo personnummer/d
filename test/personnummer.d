90c48b31d2f0aacb44412a26e381093aca180fb7
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2020-04-24 19:00:11 +0200

    Add more tests

--- test/personnummer.d
+++ test/personnummer.d
@@ -1,32 +1,86 @@
 module tests.personnummer;
 
-import personnummer;
+import std.exception;
+import core.exception : AssertError;
 import std.net.curl;
 import std.stdio;
 import std.json;
 import std.conv;
+import std.string : indexOf, replace;
+import personnummer;
 
 string[] availableListFormats = [
-    "long_format",
-    "short_format",
-    "separated_format",
-    "separated_long"
+    "long_format", "short_format", "separated_format", "separated_long"
 ];
 
 JSONValue _testList;
 JSONValue testList()
 {
-    if (_testList.toString() == "null") {
-        auto content = get("https://raw.githubusercontent.com/personnummer/meta/master/testdata/list.json");
+    if (_testList.toString() == "null")
+    {
+        auto content = get(
+                "https://raw.githubusercontent.com/personnummer/meta/master/testdata/list.json");
         _testList = parseJSON(content);
     }
     return _testList;
 }
 
-unittest {
-    foreach (i, item; testList.array()) {
-        foreach (j, format; availableListFormats) {
-            assert(item["valid"].boolean == Personnummer.valid(item[format].get!string));
+unittest
+{
+    foreach (i, item; testList.array())
+    {
+        foreach (j, format; availableListFormats)
+        {
+            assert(item["valid"].boolean == Personnummer.valid(item[format].str));
+        }
+    }
+}
+
+unittest
+{
+    foreach (i, item; testList.array())
+    {
+        if (item["valid"].boolean)
+        {
+            foreach (j, format; availableListFormats)
+            {
+                if (format != "short_format" && indexOf(item["separated_format"].str, '+') == -1)
+                {
+                    assert(item["separated_format"].str == Personnummer.parse(item[format].str)
+                            .format());
+                    assert(item["long_format"].str == Personnummer.parse(item[format].str)
+                            .format(true));
+                }
+            }
+        }
+    }
+}
+
+unittest
+{
+    foreach (i, item; testList.array())
+    {
+        if (!item["valid"].boolean)
+        {
+            foreach (j, format; availableListFormats)
+            {
+                assertThrown!PersonnummerException(Personnummer.parse(item[format].str));
+            }
+        }
+    }
+}
+
+unittest
+{
+    foreach (i, item; testList.array())
+    {
+        if (item["valid"].boolean)
+        {
+            foreach (j, format; availableListFormats)
+            {
+                assert(item["isMale"].boolean == Personnummer.parse(item[format].str).isMale());
+                assert(item["isFemale"].boolean == Personnummer.parse(item[format].str).isFemale());
+            }
         }
     }
 }
